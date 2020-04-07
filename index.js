c5cedaff1e0fbec0d2148825683f3a7eb084b2aa
GUPTASHUBH057 <56784923+GUPTASHUBH057@users.noreply.github.com>
2020-04-07 23:17:46 +0530

    initial files upload

---
+++ index.js
@@ -0,0 +1,27 @@
+document.getElementById("btn1").onclick = function() {
+  location.href = "https://www.nirfindia.org/Home";
+};
+
+document.getElementById("btn2").onclick = function() {
+  location.href = "http://www.mnit.ac.in";
+};
+
+document.getElementById("btn3").onclick = function() {
+  location.href = "https://www.aicte-india.org/";
+};
+
+document.getElementById("btn4").onclick = function() {
+  location.href = "https://nptel.ac.in/";
+};
+
+document.getElementById("btn5").onclick = function() {
+  location.href = "https://swayam.gov.in/";
+};
+
+document.getElementById("btn6").onclick = function() {
+  location.href = "https://www.w3schools.com/";
+};
+
+
+var year = new Date().getFullYear();
+document.getElementById("copyright").innerHTML += year;
